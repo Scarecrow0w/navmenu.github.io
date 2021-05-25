affea3bc4e771323aa5a672865d52a451ab339bd
Scarecrow0w <daniil6547@ya.ru>
2021-05-25 22:20:40 +0300

    initial commit

---
+++ vite/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './*.html',
+     './*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        rose: '#F2DDDE'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
