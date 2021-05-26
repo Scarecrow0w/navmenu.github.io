0aac345466018df38320e1be10cf62fcc6483321
Scarecrow0w <daniil6547@ya.ru>
2021-05-26 14:59:29 +0300

    commit

--- vite/main.js
+++ vite/main.js
@@ -1,2 +1,61 @@
 import './style.css'
 
+let submenu = document.querySelector('.submenu'),
+discountMenu = document.querySelector('.discount-image'),
+links = document.querySelectorAll('.link-tree'),
+linksNest = document.querySelector('.links-nest'),
+linkNest = document.querySelectorAll('.link-nest'),
+nestedLinks = document.querySelector('.nested-links');
+
+links.forEach((el) => {
+    el.addEventListener('mouseenter', () => {
+        discountMenu.classList.remove('flex')
+        discountMenu.classList.add('hidden')
+        linksNest.classList.add('-translate-x-3')
+        linksNest.classList.remove('flex')
+        linksNest.classList.add('hidden')
+        if (submenu.classList.contains('flex')) {
+            fadeIn(linksNest)
+        } else {
+            fadeIn(submenu)
+            linksNest.classList.remove('hidden')
+            linksNest.classList.add('flex')
+        }
+        setTimeout(() => {
+            linksNest.classList.remove('-translate-x-3')
+        }, 0)
+    })
+})
+
+linkNest.forEach((el) => {
+    el.addEventListener('mouseenter', () => {
+        nestedLinks.classList.add('-translate-x-3')
+        fadeIn(nestedLinks)
+        setTimeout(() => {
+            nestedLinks.classList.remove('-translate-x-3')
+        }, 0)
+    })
+
+    el.addEventListener('mouseleave', () => {
+        nestedLinks.classList.remove('flex')
+        nestedLinks.classList.add('hidden')
+        nestedLinks.classList.add('-translate-x-3')
+    })
+})
+
+
+// FadeIn effect
+function fadeIn(el) {
+    let op = 0
+    el.style.opacity = op
+    el.classList.remove('hidden')
+    el.classList.add('flex')
+    
+    let timer = setInterval(function () {
+      if (op >= 1) {
+        clearInterval(timer)
+      }
+      el.style.opacity = op
+      op = op + 0.1
+    }, 20);
+  }
