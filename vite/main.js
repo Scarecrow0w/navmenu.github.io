f745df9149703cc8d4aa4dff0cabeadcae9f46ba
Scarecrow0w <daniil6547@ya.ru>
2021-05-28 18:36:18 +0300

    commit

--- vite/main.js
+++ vite/main.js
@@ -1,6 +1,10 @@
 import './style.css'
 
 let submenu = document.querySelector('.submenu'),
+nav = document.querySelector('nav'),
+catalogLink = document.querySelector('.catalog-link'),
+megamenu = document.querySelector('.megamenu'),
+mouseenterMegamenu = false,
 discountMenu = document.querySelector('.discount-image'),
 links = document.querySelectorAll('.link-tree'),
 linksNest = document.querySelector('.links-nest'),
@@ -10,44 +14,73 @@ productImg = document.querySelectorAll('.product-image'),
 productDescription = document.querySelectorAll('.product-description'),
 product = document.querySelectorAll('.product');
 
+catalogLink.addEventListener('mouseenter', () => {
+    megamenu.classList.remove('hidden')
+    megamenu.classList.add('flex')
+    submenu.classList.remove('flex')
+    submenu.classList.add('hidden')
+    discountMenu.classList.remove('-translate-y-10')
+    discountMenu.classList.remove('hidden')
+    discountMenu.classList.add('flex')
+    discountMenu.style.opacity = 1
+})
+
+megamenu.onmouseenter = () => mouseenterMegamenu = true 
+megamenu.onmouseleave = () => mouseenterMegamenu = false 
+
+nav.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+        if (!mouseenterMegamenu) {
+            megamenu.classList.remove('flex')
+            megamenu.classList.add('hidden')
+        }
+    }, 10);
+})
+
 links.forEach((el) => {
     el.addEventListener('mouseenter', () => {
         nestedLinks.classList.remove('flex')
         nestedLinks.classList.add('hidden')
-        discountMenu.classList.remove('flex')
-        discountMenu.classList.add('hidden')
-        linksNest.classList.remove('flex')
-        linksNest.classList.add('hidden')
-        linksNest.classList.add('-translate-x-3')
-        product.forEach((item => {
-            item.classList.remove('flex')
-            item.classList.add('hidden')
-        }))
-        productImg.forEach((item) => item.classList.add('-translate-x-10'))
-        productDescription.forEach((item) => item.classList.add('-translate-x-16'))
-
-
-        if (submenu.classList.contains('flex')) {
-            setTimeout(() => {
-                fadeIn(linksNest)
-                // product.forEach((item) => fadeIn(item))
-            }, 0);
-        } else {
-            setTimeout(() => {
-                fadeIn(submenu)
-                // product.forEach((item) => fadeIn(item))
-                linksNest.classList.remove('hidden')
-                linksNest.classList.add('flex')
-            }, 0);
-        }
+        // discountMenu.classList.remove('flex')
+        // discountMenu.classList.add('hidden')
+        fadeOut(discountMenu)
         setTimeout(() => {
-            linksNest.classList.remove('-translate-x-3')
-            product.forEach((item) => fadeIn(item))
+            discountMenu.classList.add('-translate-y-10')
+        }, 0);
+        setTimeout(() => {
+            linksNest.classList.remove('flex')
+            linksNest.classList.add('hidden')
+            linksNest.classList.add('-translate-x-3')
+            product.forEach((item => {
+                item.classList.remove('flex')
+                item.classList.add('hidden')
+            }))
+            productImg.forEach((item) => item.classList.add('-translate-x-10'))
+            productDescription.forEach((item) => item.classList.add('-translate-x-16'))
+
+
+            if (submenu.classList.contains('flex')) {
+                setTimeout(() => {
+                    fadeIn(linksNest)
+                    // product.forEach((item) => fadeIn(item))
+                }, 0);
+            } else {
+                setTimeout(() => {
+                    fadeIn(submenu)
+                    // product.forEach((item) => fadeIn(item))
+                    linksNest.classList.remove('hidden')
+                    linksNest.classList.add('flex')
+                }, 0);
+            }
             setTimeout(() => {
-                productImg.forEach((item) => item.classList.remove('-translate-x-10'))
-                productDescription.forEach((item) => item.classList.remove('-translate-x-16'))
-            }, 50);
-        }, 150)
+                linksNest.classList.remove('-translate-x-3')
+                product.forEach((item) => fadeIn(item))
+                setTimeout(() => {
+                    productImg.forEach((item) => item.classList.remove('-translate-x-10'))
+                    productDescription.forEach((item) => item.classList.remove('-translate-x-16'))
+                }, 50);
+            }, 200)
+        }, 200);
     })
 })
 
@@ -60,7 +93,7 @@ linkNest.forEach((el) => {
             fadeIn(nestedLinks)
             setTimeout(() => {
                 nestedLinks.classList.remove('-translate-x-3')                
-            }, 150);
+            }, 200);
         }, 0)
     })
 })
@@ -74,10 +107,26 @@ function fadeIn(el) {
     el.classList.add('flex')
     
     let timer = setInterval(function () {
-      if (op >= 1) {
-        clearInterval(timer)
-      }
+      if (op >= 1) clearInterval(timer)      
       el.style.opacity = op
       op = op + 0.1
     }, 20);
   }
+
+  // FadeOut effect
+  function fadeOut(el) {
+    let op = 10;
+
+    let st = setInterval(function(){
+        op--;
+        el.style.opacity = op / 10 
+
+        if (op <= 0) {
+            clearInterval(st)
+            setTimeout(() => {
+                el.classList.remove('flex')
+                el.classList.add('hidden')  
+            }, 100);
+        }  
+    }, 10)
+}
